7bfbabb0b346197ad544c4291dc52d7154fc1436
Jarod G.R. Meng <genrong.meng@gmail.com>
2016-08-09 18:41:58 -0700

    Upgrade upload_file() to allow adding permissions with function call

--- R/upload.r
+++ R/upload.r
@@ -3,12 +3,14 @@ upload_url <- "https://www.googleapis.com/upload/drive/v2/files"
 #' upload a file to Google Drive
 #' @param file_name file name
 #' @param title file title
-#' @param type mime type of the file
+#' @param file_type mime type of the file
+#' @param email email address to add to permission list
 #' @param token a valid OAuth2.0 token
+#' @param ... any other arguments to be passed to add_permission()
 #' @return ID of the uploaded file
 #' @export
-upload_file <- function(file_name, title = file_name, type = NULL,
-                        token = get_access_cred()) {
+upload_file <- function(file_name, title = file_name, file_type = NULL,
+                        email = NULL, token = get_access_cred(), ...) {
   url <- upload_url
 
   metadata <- tempfile()
@@ -18,12 +20,17 @@ upload_file <- function(file_name, title = file_name, type = NULL,
     body = list(
       metadata = httr::upload_file(metadata,
                                    type = "application/json; charset=UTF-8"),
-      media = httr::upload_file(file_name, type = type)),
+      media = httr::upload_file(file_name, type = file_type)),
     config = config(token = token),
     add_headers("Content-Type" = "multipart/related"),
     encode = "multipart")
 
   stop_for_status(req)
   resp <- content(req)
+  
+  if (!is.null(email)) {
+    perm_id <- add_permission(resp$id, email = email, ...)
+  }
+  
   return(resp$id)
 }
