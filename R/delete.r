147fef2c6e244de4d3586b89fa9497770c89c524
Jarod G.R. Meng <genrong.meng@gmail.com>
2016-08-09 20:48:19 -0700

    Add trash_files_from_title() function

--- R/delete.r
+++ R/delete.r
@@ -10,3 +10,22 @@ trash_file <- function(file_id, token = get_access_cred()) {
   resp <- content(req)
   resp$id
 }
+
+#' Move file(s) to trash
+#' @export
+#' @param title file title
+#' @param all whether to move all file(s) having the same title to trash
+#' @param token a valid OAuth2.0 token
+#' @return ID(s) of file(s) moved to trash
+trash_files_from_title <- function(title, all = FALSE,
+                                   token = get_access_cred()) {
+  files <- list_files_from_title(title, token)
+  
+  if (!all) {
+    file_id <- files$items[[1]]$id
+    return(trash_file(file_id, token))
+  } else {
+    file_ids <- sapply(files$items, function(x) x$id)
+    return(sapply(file_ids, trash_file, token = token))
+  }
+}
